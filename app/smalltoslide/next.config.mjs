a658491a3d0462ab6780813aea9f936430226d07
Elliot Kang <kkang2097@gmail.com>
2024-08-17 17:36:25 -0700

    milvus hack

--- app/smalltoslide/next.config.mjs
+++ app/smalltoslide/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {experimental: {
-    serverComponentsExternalPackages: ["llamaindex"], // Puts LlamaIndex in actual NodeJS mode with NextJS App Router
+    serverComponentsExternalPackages: ["llamaindex", "@zilliz/milvus2-sdk-node"], // Puts LlamaIndex in actual NodeJS mode with NextJS App Router
   }};
 
 export default nextConfig;
