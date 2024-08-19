09ff04e03906323915af7de51630dca64dfedf67
Elliot Kang <kkang2097@gmail.com>
2024-08-19 00:27:56 -0700

    Update route.ts

--- app/smalltoslide/app/api/data/route.ts
+++ app/smalltoslide/app/api/data/route.ts
@@ -1,15 +1,16 @@
 import {query} from '@/rag/query_to_chunk';
-
+import {ChunkBundle} from '@/rag/types';
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 
 
 export async function GET(request: Request) {
-  const result = await query();
+  const result: ChunkBundle = await query();
 
-  if (typeof result != "string"){
-    console.log("Query failed.")
-  }
+  // if (typeof result != "object"){
+  //   console.log("Query failed.")
+  // }
+
   //return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   return new Response(JSON.stringify(result));
 }
