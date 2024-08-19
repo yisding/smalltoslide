bb4364571fe97038760df786c5cd495250fc5d5b
Elliot Kang <kkang2097@gmail.com>
2024-08-19 00:51:16 -0700

    generate a POST request

--- app/smalltoslide/app/api/data/route.ts
+++ app/smalltoslide/app/api/data/route.ts
@@ -1,15 +1,37 @@
 import {query} from '@/rag/queryToChunk';
-import {ChunkBundle} from '@/rag/types';
+import {ChunkBundle, DocumentOption, RagRequestJSON} from '@/rag/types';
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 
+// This is the legacy GET request
 
-export async function GET(request: Request) {
-  const result: ChunkBundle = await query();
+// export async function GET(request: Request) {
 
-  // if (typeof result != "object"){
-  //   console.log("Query failed.")
-  // }
+//   const result: ChunkBundle = await query();
+
+//   // if (typeof result != "object"){
+//   //   console.log("Query failed.")
+//   // }
+
+//   //return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+//   return new Response(JSON.stringify(result));
+// }
+
+
+
+
+// This is the resulting POST request derived from the GET request
+export async function POST(request: Request) {
+
+  // Example of a dummy request JSON
+  // const reqJSON = {
+  //     query: "How is NVDA doing?",
+  //     option: DocumentOption.Nvidia
+  // } as RagRequestJSON;
+
+  const reqJSON: RagRequestJSON = await request.json();
+
+  const result: ChunkBundle = await query(reqJSON.query, reqJSON.option);
 
   //return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   return new Response(JSON.stringify(result));
