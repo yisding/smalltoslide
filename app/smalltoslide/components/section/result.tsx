6d12e008f8f6cb922de4cccd3ca7dc26fa394a9a
Yi Ding <yi.s.ding@gmail.com>
2024-09-17 15:46:25 -0700

    small fixes

--- app/smalltoslide/components/section/result.tsx
+++ app/smalltoslide/components/section/result.tsx
@@ -28,13 +28,13 @@ export function Result({ images, response, context }: ResultProps) {
         <>
           <h4 className="my-2">Retrieved Context:</h4>
           <h4 className="font-semibold">Response:</h4>
-          <pre className="text-sm">{context}</pre>
+          <pre className="text-wrap text-sm">{context}</pre>
         </>
       )}
 
       {response && (
         <>
-          <h4 className="font-semibold">Response:</h4>
+          <h4 className="pt-4 font-semibold">Response:</h4>
           <div className="text-sm">{response}</div>
         </>
       )}
